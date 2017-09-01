e625d266c816c8790a7e923dc8eacec63ff7d10f
tanaka_733 <tatanaka@redhat.com>
2017-09-01 12:26:42 +0900

    Update ResultViewModel.cs

--- Models/HomeViewModels/ResultViewModel.cs
+++ Models/HomeViewModels/ResultViewModel.cs
@@ -9,28 +9,28 @@ namespace WebApplication.Models.HomeViewModels
         [DataType(DataType.Url)]
         public string Url { get; set; }
 
-        [Display(Name = "怒り")]
+        [Display(Name = "Anger(怒り)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Anger { get; set; }
-        [Display(Name = "軽蔑")]
+        [Display(Name = "Contempt(軽蔑)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Contempt { get; set; }
-        [Display(Name = "ムカつき")]
+        [Display(Name = "Disgust(ムカつき)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Disgust { get; set; }
-        [Display(Name = "恐れ")]
+        [Display(Name = "Fear(恐れ)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Fear { get; set; }
-        [Display(Name = "喜び")]
+        [Display(Name = "Happiness(喜び)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Happiness { get; set; }
-        [Display(Name = "無表情")]
+        [Display(Name = "Neutral(無表情)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Neutral { get; set; }
-        [Display(Name = "悲しみ")]
+        [Display(Name = "Sadness(悲しみ)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Sadness { get; set; }
-        [Display(Name = "驚き")]
+        [Display(Name = "Surprise(驚き)")]
         [DisplayFormat(DataFormatString = "{0:0.000000}")]
         public double Surprise { get; set; }
     }
